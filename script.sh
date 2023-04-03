fbfad77931a5fb080f33b737ea2c43549135720a
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-04-03 19:43:07 +0200

    skrypt tworzacy 100 plikow log z zawartoscia.

--- script.sh
+++ script.sh
@@ -1,1 +1,15 @@
-date +%c
+#!/bin/bash
+
+#skrypt tworzący 30 plików
+
+data=$(date)
+nazwa_skryptu=$(basename "$0")
+
+if [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++)); do
+      touch log$i.txt
+      echo $data > log$i.txt
+      echo log$i.txt >> log$i.txt
+      echo $nazwa_skryptu >> log$i.txt
+    done
+fi
