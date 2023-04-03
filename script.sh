f4c6f1763b1c02a590d0ba1c1deb03ea32ecf935
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2023-04-03 19:54:57 +0200

    dodano --help.

--- script.sh
+++ script.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-#skrypt tworzący 30 plików
-
-data=$(date)
-nazwa_skryptu=$(basename "$0")
-
-if [ "$1" = "--logs" ]; then
-  if [ "$2" -gt 0 ] 2>/dev/null; then
-    for ((i=1; i<=$2; i++)); do
-      touch log$i.txt
-      echo $data > log$i.txt
-      echo log$i.txt >> log$i.txt
-      echo $nazwa_skryptu >> log$i.txt
-    done
-  fi
+if [ "$1" = "--help" ]; then
+    echo --help wyswietla liste parametrow do uzycia
+    echo --date wyswietla date dla danej strefy czasowej
+    echo --log tworzy 100 plikow logx.txt gdzie x to kazdy kolejny numer pliku
+    echo --log liczba tworzy 100 plikow logx.txt gdzie x to kazdy kolejny numer pliku liczba to ilosc plikow jakie ma stworzyc skrypt
 fi
